ae9ebe1b1385091c83a639d2141bcbbcdd667cc9
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-02-14 15:06:27 +0100

    Create blip_model.py

---
+++ models/blip_model.py
@@ -0,0 +1,27 @@
+from transformers import BlipProcessor, BlipForConditionalGeneration
+import torch
+
+HF_TOKEN =(the token code) 
+processor = BlipProcessor.from_pretrained(
+    "Salesforce/blip-image-captioning-base",
+    token=HF_TOKEN
+)
+
+model = BlipForConditionalGeneration.from_pretrained(
+    "Salesforce/blip-image-captioning-base",
+    token=HF_TOKEN
+)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
+model.eval()
+
+def generate_caption(image_path):
+    """Generate caption for a given image path."""
+    from PIL import Image
+    image = Image.open(image_path).convert("RGB")
+    inputs = processor(image, return_tensors="pt").to(device)
+    with torch.no_grad():
+        out = model.generate(**inputs, max_new_tokens=50)
+    caption = processor.decode(out[0], skip_special_tokens=True)
+    return caption
