405af99e52fac0d90eec6ff69e34322bba8349c1
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-02-14 15:09:53 +0100

    open_image function

--- gui/app_gui.py
+++ gui/app_gui.py
@@ -18,30 +18,3 @@ def open_image(panel, caption_label):
         caption = generate_caption(file_path)
         caption_label.config(text=caption)
 
-def start_gui():
-    """Create and run the Tkinter GUI."""
-    root = tk.Tk()
-    root.title("BLIP Image Captioning")
-
-    bg_image = Image.open("assets/ezerbouz.jpg")
-    bg_photo = ImageTk.PhotoImage(bg_image)
-    bg_label = tk.Label(root, image=bg_photo)
-    bg_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-    top_text = tk.Label(root,
-                        text="Hi! Ezer is with you, ready to describe any image you choose!",
-                        font=("Arial", 16, "bold"), bg="#000000", fg="white")
-    top_text.pack(pady=10)
-
-    panel = tk.Label(root, bg="#000000")
-    panel.pack(pady=10)
-
-    caption_label = tk.Label(root, text="", wraplength=400, font=("Arial", 12),
-                             bg="#000000", fg="white")
-    caption_label.pack(pady=10)
-
-    btn = tk.Button(root, text="Choisir une image",
-                    command=lambda: open_image(panel, caption_label))
-    btn.pack(pady=10)
-
-    root.mainloop()
