943726208da5a3f98c19660a4b5625fa10ae5cc7
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-02-16 17:20:06 +0100

    Image file handler

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -3,8 +3,7 @@ from PIL import Image, ImageTk
 
 def open_image_file():
     file_path = filedialog.askopenfilename(
-        filetypes=[("Image files", "*.jpg *.jpeg *.png *.bmp")]
-    )
+        filetypes=[("Image files", "*.jpg *.jpeg *.png *.bmp")])
     return file_path
 
 def load_image_thumbnail(path, size=(400, 400)):
