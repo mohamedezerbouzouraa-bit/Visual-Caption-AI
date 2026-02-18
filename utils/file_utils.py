0d63b1fe5cf4bbdd069782f861b70a52b6e2207e
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-02-18 09:17:24 +0100

    Image file handler

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -5,7 +5,6 @@ def open_image_file():
     file_path = filedialog.askopenfilename(
         filetypes=[("Image files", "*.jpg *.jpeg *.png *.bmp")])
     return file_path
-
 def load_image_thumbnail(path, size=(400, 400)):
     image = Image.open(path)
     image.thumbnail(size)
