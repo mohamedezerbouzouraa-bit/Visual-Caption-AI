51b903b2800800ad5f0f59b44425326f1a14e005
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-02-18 09:17:45 +0100

    Loading BLIP model and generating captions

--- models/blip_model.py
+++ models/blip_model.py
@@ -5,11 +5,9 @@ HF_TOKEN =(the token code)
 processor = BlipProcessor.from_pretrained(
     "Salesforce/blip-image-captioning-base",
     token=HF_TOKEN)
-
 model = BlipForConditionalGeneration.from_pretrained(
     "Salesforce/blip-image-captioning-base",
     token=HF_TOKEN)
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
 model.eval()
