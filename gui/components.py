105d1c9513da5aefceb396ecbc00e22dee0de305
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-02-19 23:04:04 +0100

    Custom widget creators

--- gui/components.py
+++ gui/components.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-
 def create_label(root, text="", font=("Arial", 12), bg="#000000", fg="white", wraplength=None):
     return tk.Label(root, text=text, font=font, bg=bg, fg=fg, wraplength=wraplength)
 def create_button(root, text, command):
