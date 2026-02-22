dec6225d4e6952161ca6c0c65f4004101a2dad95
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-02-22 08:08:02 +0100

    Image file handler

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,10 +1,12 @@
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
+
 def open_image_file():
     file_path = filedialog.askopenfilename(
         filetypes=[("Image files", "*.jpg *.jpeg *.png *.bmp")])
     return file_path
+
 
 def load_image_thumbnail(path, size=(400, 400)):
     image = Image.open(path)
